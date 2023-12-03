adb7f6389b1dbbbc8075a0be8f239d60ff464d35
jesse-gonzalez <jesse.gonzalez@nutanix.com>
2023-12-03 18:26:31 -0500

    fix: docker build image names

--- .mk/docker.mk
+++ .mk/docker.mk
@@ -7,16 +7,16 @@ TARGET_RELEASE ?= $(GIT_LATEST_TAG)
 
 .PHONY: docker-clean
 docker-clean: #### Remove Calm DSL Util Image locally with necessary tools to develop and manage Cloud-Native Apps (e.g., kubectl, argocd, git, helm, helmfile, etc.)
-	docker image ls --filter "reference=${IMAGE_REGISTRY_ORG}/cloud-native-calm-utils" --format "{{.Repository}}:{{.Tag}}" | xargs -I {} docker rmi -f {}
+	docker image ls --filter "reference=${IMAGE_REGISTRY_ORG}/nutanix-cloud-native-utils" --format "{{.Repository}}:{{.Tag}}" | xargs -I {} docker rmi -f {}
 
 .PHONY: docker-build
 docker-build: docker-clean #### Build Calm DSL Util Image locally with necessary tools to develop and manage Cloud-Native Apps (e.g., kubectl, argocd, git, helm, helmfile, etc.)
-	cd .devcontainer && docker build -t ${IMAGE_REGISTRY_ORG}/cloud-native-calm-utils:latest .
-	docker tag ${IMAGE_REGISTRY_ORG}/cloud-native-calm-utils ${IMAGE_REGISTRY_ORG}/cloud-native-calm-utils:$(TARGET_RELEASE)
+	cd .devcontainer && docker build -t ${IMAGE_REGISTRY_ORG}/nutanix-cloud-native-utils:latest .
+	docker tag ${IMAGE_REGISTRY_ORG}/nutanix-cloud-native-utils ${IMAGE_REGISTRY_ORG}/nutanix-cloud-native-utils:$(TARGET_RELEASE)
 
 .PHONY: docker-run
 docker-run: ### Launch into Calm DSL development container. If image isn't available, build will auto-run
-	[ -n "$$(docker image ls ${IMAGE_REGISTRY_ORG}/cloud-native-calm-utils:$(TARGET_RELEASE) -q)" ] || make docker-build
+	[ -n "$$(docker image ls ${IMAGE_REGISTRY_ORG}/nutanix-cloud-native-utils:$(TARGET_RELEASE) -q)" ] || make docker-build
 	docker run --rm -it \
 		--network ${DOCKER_NETWORK} \
 		-v /var/run/docker.sock:/var/run/docker.sock \
@@ -24,7 +24,7 @@ docker-run: ### Launch into Calm DSL development container. If image isn't avail
 		-w '/dsl-workspace' \
 		-e ENVIRONMENT='${ENVIRONMENT}' \
 		-e GITGUARDIAN_API_KEY='${GITGUARDIAN_API_KEY}' \
-		${IMAGE_REGISTRY_ORG}/cloud-native-calm-utils /bin/sh -c "make help && ${DEFAULT_SHELL}"
+		${IMAGE_REGISTRY_ORG}/nutanix-cloud-native-utils /bin/sh -c "make help && ${DEFAULT_SHELL}"
 
 .PHONY: docker-login
 docker-login: #### Login to Github Private Registry
@@ -32,12 +32,12 @@ docker-login: #### Login to Github Private Registry
 
 .PHONY: docker-push
 docker-push: docker-login ## Tag and Push latest image and short sha version to desired image repo.
-	[ -n "$$(docker image ls ${IMAGE_REGISTRY_ORG}/cloud-native-calm-utils -q)" ] || make docker-build
-	@docker push ${IMAGE_REGISTRY_ORG}/cloud-native-calm-utils:latest
-	@docker tag ${IMAGE_REGISTRY_ORG}/cloud-native-calm-utils ${IMAGE_REGISTRY_ORG}/cloud-native-calm-utils:$(GIT_COMMIT_ID)
-	@docker push ${IMAGE_REGISTRY_ORG}/cloud-native-calm-utils:$(GIT_COMMIT_ID)
+	[ -n "$$(docker image ls ${IMAGE_REGISTRY_ORG}/nutanix-cloud-native-utils -q)" ] || make docker-build
+	@docker push ${IMAGE_REGISTRY_ORG}/nutanix-cloud-native-utils:latest
+	@docker tag ${IMAGE_REGISTRY_ORG}/nutanix-cloud-native-utils ${IMAGE_REGISTRY_ORG}/nutanix-cloud-native-utils:$(GIT_COMMIT_ID)
+	@docker push ${IMAGE_REGISTRY_ORG}/nutanix-cloud-native-utils:$(GIT_COMMIT_ID)
 
 .PHONY: docker-release
 docker-release: docker-login ## Tag and Push latest release to desired image repo.
-	@docker tag ${IMAGE_REGISTRY_ORG}/cloud-native-calm-utils ${IMAGE_REGISTRY_ORG}/cloud-native-calm-utils:$(GIT_LATEST_TAG)
-	@docker push ${IMAGE_REGISTRY_ORG}/cloud-native-calm-utils:$(GIT_LATEST_TAG)
+	@docker tag ${IMAGE_REGISTRY_ORG}/nutanix-cloud-native-utils ${IMAGE_REGISTRY_ORG}/nutanix-cloud-native-utils:$(GIT_LATEST_TAG)
+	@docker push ${IMAGE_REGISTRY_ORG}/nutanix-cloud-native-utils:$(GIT_LATEST_TAG)
