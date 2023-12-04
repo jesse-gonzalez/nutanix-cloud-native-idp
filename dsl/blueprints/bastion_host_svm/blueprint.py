48ba2d0641361878f69fd0b20ce7ed85d452677e
jesse-gonzalez <jesse.gonzalez@nutanix.com>
2023-12-04 01:00:22 +0000

    fix: naming standards

--- dsl/blueprints/bastion_host_svm/blueprint.py
+++ dsl/blueprints/bastion_host_svm/blueprint.py
@@ -186,11 +186,11 @@ class Bastion_HostProfile(VmProfile):
         #     cred=NutanixCred
         # )
 
-        CalmTask.Exec.ssh(
-            name="Install Kube PS1",
-            filename="../../_common/linux/scripts/install_kube-ps1.sh",
-            cred=NutanixCred
-        )
+        # CalmTask.Exec.ssh(
+        #     name="Install Kube PS1",
+        #     filename="../../_common/linux/scripts/install_kube-ps1.sh",
+        #     cred=NutanixCred
+        # )
 
         # CalmTask.Exec.ssh(
         #     name="Install Kubectl Krew Package Manager",
